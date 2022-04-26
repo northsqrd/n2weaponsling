b75a3f0e13b9fd2156b88af55039443d36407162
northsqrd <41840853+northsqrd@users.noreply.github.com>
2022-04-27 01:37:07 +0200

    chore: add some comments to the config.

--- config_c.lua
+++ config_c.lua
@@ -1,10 +1,10 @@
 cfg = {}
 
 cfg.attachables = {
-    [`weapon_carbinerifle`] = {
-        model = `w_ar_carbinerifle`,
-        attachBone = 24818,
-        defaultLoc = "front",
+    [`weapon_carbinerifle`] = {  -- The hashkey of the weapon itself.
+        model = `w_ar_carbinerifle`, -- The hashkey of the weapon model that gets attached.
+        attachBone = 24818, -- Which bone you want to attach the weapon to. See https://wiki.gtanet.work/index.php?title=Bones for a list.
+        defaultLoc = "front", -- The default location the weapon attaches to. Must match a value of one of the options in the "loc" table.
         loc = {
             ["front"] = {
                 pos = vector3(-0.07, 0.23, -0.001),
@@ -16,9 +16,9 @@ cfg.attachables = {
             }
         },
         components = {
-            [`component_carbinerifle_clip_01`] = {
-                attachBone = 'WAPClip',
-                model = `w_ar_carbinerifle_mag1`
+            [`component_carbinerifle_clip_01`] = { -- The hashkey of the component.
+                attachBone = 'WAPClip', -- Which bone of the weapon you want to attach the component to. This is found in the weapons.meta under AttachPoints > > Item > AttachBone.
+                model = `w_ar_carbinerifle_mag1` -- The hashkey of the component model that gets attached.
             },
             [`component_carbinerifle_clip_02`] = {
                 attachBone = 'WAPClip',
